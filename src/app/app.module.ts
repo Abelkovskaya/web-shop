87a6cc2920be0c82652d32e81e6da147b8ebff09
Anna Belkovskaya <anisia-baik@list.ru>
2024-08-22 21:34:36 +0200

    menu hidden on click outside added on Cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ClickOutsideDirective } from './cart-page/clickOutside.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-
+    ClickOutsideDirective,
+    AppComponent
   ],
   imports: [
-
+    HttpClientModule, BrowserModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, VERSION, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
 export class CartPageComponent {
+  menuOpen: boolean = false;
+  menuBtnClick: boolean = false;
 
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (!this.menuBtnClick) {
+        this.menuOpen = false;
+      }
+      this.menuBtnClick = false;
+    });
+  }
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  preventCloseOnClick() {
+    this.menuBtnClick = true;
+  }
 }
