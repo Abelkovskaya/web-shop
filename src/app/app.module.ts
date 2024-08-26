b67f36df8b8df7cad5e76de8f86d108b6a2ade89
Anna Belkovskaya <anisia-baik@list.ru>
2024-08-26 16:19:26 +0200

    Last updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ClickOutsideDirective } from './cart-page/clickOutside.directive';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    ClickOutsideDirective,
     AppComponent
   ],
   imports: [
-    HttpClientModule, BrowserModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
